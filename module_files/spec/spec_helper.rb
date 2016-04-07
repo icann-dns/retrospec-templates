e93abe565e4320f13ff3c8b13bcb11e96ac4cb51
b4ldr <github@johnbond.org>
2016-04-07 12:49:02 -0300

    add rspec-puppet-facts

--- module_files/spec/spec_helper.rb
+++ module_files/spec/spec_helper.rb
@@ -2,12 +2,14 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-utils'
 # if your using puppet4, the following gem seems to causes issues
 require 'hiera-puppet-helper'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
 
 # Uncomment this to show coverage report, also useful for debugging
-#at_exit { RSpec::Puppet::Coverage.report! }
+at_exit { RSpec::Puppet::Coverage.report! }
 
 #set to "yes" to enable the future parser, the equivalent of setting parser=future in puppet.conf.
-#ENV['FUTURE_PARSER'] = 'yes'
+ENV['FUTURE_PARSER'] = 'yes'
 
 # set to "yes" to enable strict variable checking, the equivalent of setting strict_variables=true in puppet.conf.
 #ENV['STRICT_VARIABLES'] = 'yes'
@@ -19,7 +21,7 @@ require 'hiera-puppet-helper'
 
 # set to "no" to enable structured facts, otherwise leave unset to retain the current default behavior.
 # This is equivalent to setting stringify_facts=false in puppet.conf.
-#ENV['STRINGIFY_FACTS']  = 'no'
+ENV['STRINGIFY_FACTS']  = 'no'
 
 # set to "yes" to enable the $facts hash and trusted node data, which enabled $facts and $trusted hashes.
 # This is equivalent to setting trusted_node_data=true in puppet.conf.
